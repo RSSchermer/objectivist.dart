beecfae3deb67c69541b78f352fd6e0cbe8298d2
R.S. Schermer <roland0507@gmail.com>
2016-11-22 22:58:16 +0100

    Implement mtl reading functions

--- lib/mtl_reading.dart
+++ lib/mtl_reading.dart
@@ -1,3 +1,59 @@
-library obj_reading;
+library mtl_reading;
 
+import 'dart:async';
 
+import 'package:resource/resource.dart';
+
+import 'mtl_reading/lexing.dart';
+import 'mtl_reading/statementizing.dart';
+
+MtlStatementizerResults statementizeMtl(String source) {
+  final lexer = new MtlLexer();
+  final statementizer = new MtlStatementizer();
+
+  for (var i = 0; i < source.length; i++) {
+    lexer.process(source.codeUnitAt(i));
+  }
+
+  lexer.process(3); // Signal "end of text"
+
+  for (var token in lexer.flush()) {
+    statementizer.process(token);
+  }
+
+  return statementizer.flush();
+}
+
+Future<MtlStatementizerResults> statementizeMtlResource(Resource resource) {
+  return resource.readAsString().then((source) => statementizeMtl(source));
+}
+
+Stream<MtlStatementizerResults> statementizeMtlResourceStreamed(
+    Resource resource) {
+  final lexer = new MtlLexer();
+  final statementizer = new MtlStatementizer();
+  final outStreamController = new StreamController<MtlStatementizerResults>();
+
+  resource.openRead().forEach((chunk) {
+    for (var i = 0; i < chunk.length; i++) {
+      lexer.process(chunk[i]);
+    }
+
+    for (var token in lexer.flush()) {
+      statementizer.process(token);
+    }
+
+    outStreamController.add(statementizer.flush());
+  }).whenComplete(() {
+    lexer.process(3); // Signal "end of text"
+
+    for (var token in lexer.flush()) {
+      statementizer.process(token);
+    }
+
+    outStreamController.add(statementizer.flush());
+    outStreamController.close();
+  });
+
+  return outStreamController.stream;
+}
